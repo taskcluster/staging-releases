f7a900b448604267be6d9c295cbcdc63cff9e322
Pete Moore <pmoore@mozilla.com>
2023-05-15 11:26:27 +0200

    wip

--- workers/generic-worker/aws_test.go
+++ workers/generic-worker/aws_test.go
@@ -331,3 +331,38 @@ func TestDeploymentIDNotUpdated(t *testing.T) {
 		t.Fatalf("Was expecting deploymentIDUpdated() function to see that deployment ID was not updated")
 	}
 }
+
+func TestWorkerShutdown(t *testing.T) {
+	test := GWTest(t)
+	ec2 := test.MockEC2() // fetch config from worker manager / taskcluster secrets
+	ec2.Terminating = true
+	err := test.Setup()
+	defer test.Teardown()
+	ExpectNoError(t, err)
+	wm := serviceFactory.WorkerManager(nil, "http://localhost:13243")
+	_, err = wm.CreateWorkerPool(
+		test.Config.ProvisionerID+"/"+test.Config.WorkerType,
+		&tcworkermanager.WorkerPoolDefinition{
+			Config: json.RawMessage(`{
+				"launchConfigs": [
+					{
+						"workerConfig": {
+							"genericWorker": {
+								"config": {
+									"deploymentId": "` + test.Config.DeploymentID + `"
+								}
+							}
+						}
+					}
+				]
+			}`),
+		},
+	)
+	ExpectNoError(t, err)
+	payload := GenericWorkerPayload{
+		Command:    sleep(20),
+		MaxRunTime: 15,
+	}
+	td := testTask(t)
+	_ = submitAndAssert(t, td, payload, "exception", "worker-shutdown")
+}
